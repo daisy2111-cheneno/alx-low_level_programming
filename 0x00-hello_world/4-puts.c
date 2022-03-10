9d2477a67f98d0f9fea85ccfce8666c101107abd
Daisy Rono <dronoh8@gmail.com>
2022-03-10 02:07:28 -0800

    Print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,21 @@
 /**
- *ALX introduction to C
  *
- *Basics
+ *
+ *
  */
+
 
 #include <stdio.h>
-/**
- *main -print using puts not printf
- *
- *Return: ALways 0 (Success)
- */
 
+
+/**
+ *main - prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ *Return:Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+	return (0);
 }
