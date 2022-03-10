fb25ae885eed3e3bab97d1968a8a4706f0771732
Daisy Rono <dronoh8@gmail.com>
2022-03-10 02:33:26 -0800

    betty am done with you

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,22 @@
 /**
- * Date:10th March 2022
- *
- * Created by: Daisy Rono
+ * File: 4-puts.c
+ * Created by :Daisy Rono
  */
 
 
 #include <stdio.h>
 
-
 /**
- * main - prints exactly \"Programming is like building a multilingual puzzle, followed by a new line.
- *
- *
- * Return:Always 0 (Success)
+ * main - Prints Programming is like building a multilingual
+ *                 puzzle, followed by a new line.
+ * 
+ * Return: 0 if successful, non-zero indictes failure
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
+
