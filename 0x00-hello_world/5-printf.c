5410e91076f6d8b7ff5bcd691715d47787891251
Daisy Rono <dronoh8@gmail.com>
2022-03-10 12:28:37 -0800

    betty again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-/*betty coding style*/
+/* Betty code style*/
 #include <stdio.h>
-
 /**
  *  * main - Prints "with proper grammar, but the outcome is
  *   *                a piece of art," followed by a new line.
  *    *
  *     * Return: Always 0.
- */      
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
