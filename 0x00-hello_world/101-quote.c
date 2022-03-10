4f03b77c63726e2af504743addb8a344aa6f679f
Daisy Rono <dronoh8@gmail.com>
2022-03-10 13:29:47 -0800

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-/*betty styling code*/
+/*betty coding style*/
 #include <stdio.h>
 #include <unistd.h>
 
@@ -7,7 +7,7 @@
  *   * - Dora Korpar, 2015-10-19",
  *    * followed by a new line, to the standard error.
  *     * Return: Always 0 (Success)
- */      
+ */
 int main(void)
 {
 		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
