f44fdc1eea8ff6708220fb15ed79013d67495bd4
Daisy Rono <dronoh8@gmail.com>
2022-03-10 08:22:05 -0800

    Betty styling code challenge

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,16 @@
 /**
- * File: 4-puts.c
- * Created by :Daisy Rono
- */
-
+ *  * File-4-puts.c
+ *   * Date:10th March 2022
+ *    */
 
 #include <stdio.h>
 
 /**
- * main - Prints Programming is like building a multilingual
- *                 puzzle, followed by a new line.
- * 
- * Return: 0 if successful, non-zero indictes failure
- */
-
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *    */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
-
