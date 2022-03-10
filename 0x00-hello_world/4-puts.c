975ced93606f0fde4e4062cbbbcd528ec37d3530
Daisy Rono <dronoh8@gmail.com>
2022-03-10 11:22:18 -0800

    Betty coding style challenge

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  *  * main - Prints "Programming is like building a multilingual
  *   *                puzzle, followed by a new line.
  *    *
- *     * Return: Always 0.
- *      */
+ *     * Return: Always 0.      
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
