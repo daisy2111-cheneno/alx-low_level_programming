f92dbb25b9633e5e57f718f689c75bb6a47c03d5
Daisy Rono <dronoh8@gmail.com>
2022-03-15 06:46:33 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
 
 /**
  *  * print_sign - prints the sign of a number
- *   * @n input to check
- *    * Return: 1 and prints + if n>0
+ *   * @n: the int to check
+ *    *  Return: 1 and prints + if n>0
  *      * returns 0 and prints zero if n=0
  *        *returns -1 and prints - if n<0.
 */
