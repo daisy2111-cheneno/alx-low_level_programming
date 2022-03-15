83097efac7ff6072bf194f6f6db47486b6687175
Daisy Rono <dronoh8@gmail.com>
2022-03-15 04:09:06 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,8 @@
 #include "main.h"
 
 /**
- *  * main -print _putchar followed by a new line
- *    *
- *      * Return :Always 0 (Success)
+ *  * main -  prints _putchar, followed by a new line
+ *   * Return: Always 0 (Success)
 */
 int main(void)
 {
