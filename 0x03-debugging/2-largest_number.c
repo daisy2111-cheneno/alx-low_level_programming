6778188f57245cf8494d8f3a79c8e159295b84d2
Daisy Rono <dronoh8@gmail.com>
2022-03-16 01:14:16 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,7 +16,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
+	else if (b > a && a < c)
 	{
 		largest = c;
 	}
@@ -24,10 +24,10 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else 
+	else
 	{
 		largest = c;
 	}
-
+	
 	return (largest);
 }
