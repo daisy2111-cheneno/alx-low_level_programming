e9cc823def0d8bc4a8299bc556023103b483129e
Daisy Rono <dronoh8@gmail.com>
2022-03-17 02:13:23 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check the multiple of two integers
+ * @a and @b are the inputs
+ * Description: a * b
+ * Return: 0 Always
+*/
+int mul(int a, int b)
+{
+	int multiple;
+
+	multiple = a * b;
+	return (multiple);
+}
