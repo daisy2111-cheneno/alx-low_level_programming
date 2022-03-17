03892e1b845a70133a34dcd271a729cec190efd3
Daisy Rono <dronoh8@gmail.com>
2022-03-17 07:05:21 -0700

    commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,29 +9,30 @@
 
 int main(void)
 {
-	long int i, n, max;
-	
+	long int i, max, n;
+
 	n = 612852475143;
 	max = -1;
 
 	while (n % 2 == 0)
 	{
-		printf("%d", 2);
-		n = n / 2;
+		max = 2;
+		n = n /2;
 	}
 
-	for (i = 3; i <= sqrt(n); i = i + 2)
+	for (i = 3; i <= n / 2; i = i + 2)
 	{
 		while (n % i == 0)
 		{
 			max = i;
-			n = n / i;
+			n = n/ i;
 		}
 	}
 
 	if (n > 2)
+	{
 		max = n;
-
+	}
 	printf("%ld\n", max);
 
 	return (0);
