d22bfeef745ed1b723e74e28c12fc770e311bfaa
Daisy Rono <dronoh8@gmail.com>
2022-03-17 02:59:06 -0700

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- *  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
- *   * 
- *    * Return: Always 0.
+ *  * print_numbers - prints the numbers, from 0 to 9,
+ *   * followed by a new line
 */
 void print_numbers(void)
 {
@@ -13,6 +12,6 @@ void print_numbers(void)
 	{
 		_putchar(i + '0');
 	}
-
+	
 	_putchar('\n');
 }
