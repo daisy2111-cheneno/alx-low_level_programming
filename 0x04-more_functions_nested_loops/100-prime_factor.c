d9d2c9910cb8aeae40b59574009ff44da7f6e7e1
Daisy Rono <dronoh8@gmail.com>
2022-03-17 06:56:39 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - check code
+ *
+ * Return 0: Always (Success)
+*/
+
+int main(void)
+{
+	long int i, n, max;
+	
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
+	{
+		printf("%d", 2);
+		n = n / 2;
+	}
+
+	for (i = 3; i <=sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
+	return (0);
+}
