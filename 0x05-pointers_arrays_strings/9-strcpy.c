df31b84a68ec79c33aad2a7a5927e2026983f03c
Daisy Rono <dronoh8@gmail.com>
2022-03-21 06:28:21 -0700

    strcp

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	    size_t i;
+
+	    
+	    for (i = 0; src[i] != '\0'; i++)
+	    {
+		    dest[i] = src[i];
+	    }
+	    dest[i] = '\0';
+	    
+	    return dest;
+}
+ 
