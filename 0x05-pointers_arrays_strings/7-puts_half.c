1fe4b4d3dde58ea6e7768490866433ceb9f0b5a2
Daisy Rono <dronoh8@gmail.com>
2022-03-21 05:17:13 -0700

    second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+*/
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i >= len /2)
+		{
+			putchar(str[i]);
+		}
+	}
+	putchar('\n');
+}
